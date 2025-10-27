36a3a18189d82b7c1dee7568f5e1ac7f711eda21
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-10-27 16:34:06 +0100

    adding admin & userpage

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,16 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/Profile', name: 'user_page')]
+    public function userPage(): Response
+    {
+        return new Response("<h1>User Page</h1>");
+    }
+
+    #[Route(path: '/Dashboard', name: 'admin_page')]
+    public function adminPage(): Response
+    {
+        return new Response("<h1>Admin Page</h1>");
+    }
 }
